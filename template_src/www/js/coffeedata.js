cb676cb31809ad229b300597f5c212bcb619e2ce
Clare Jung <clarejung02>
2020-11-17 19:59:41 -0800

    Added new files

---
+++ template_src/www/js/coffeedata.js
@@ -0,0 +1,141 @@
+// resort list data
+var menuSet = [
+    {
+        id: 0, 
+        name: 'Espresso', 
+        active: true, 
+        keyImagePath:'./img/menu/espresso.jpg', 
+        shortDesc: 'Our signature Espresso Bean with rich flaover and cocoa sweetness is the best fresh esspresso',
+        calories: '10',
+        totalFat: '0',
+        totalCarb: '2',
+        nutritionDesc: 'Adults and youth(ages 13 and older) need ann average of 2,000 calories a day, and childerne(ages 4 to 12) need an average of 1,500 calories a day. However, individual needs vary.',
+        price: '$2.50'
+    },
+    {
+        id: 1, 
+        name: 'Espresso Macchiato', 
+        active: true, 
+        keyImagePath:'./img/menu/espesso-macchiato.jpg', 
+        shortDesc: 'Espresso meats a steam form to enhance the rich and creamy milk flavours of a stright-up shot',
+        calories: '25',
+        totalFat: '1',
+        totalCarb: '3',
+        nutritionDesc: 'Adults and youth(ages 13 and older) need ann average of 2,000 calories a day, and childerne(ages 4 to 12) need an average of 1,500 calories a day. However, individual needs vary.',
+        price: '$3.20'
+    },
+    {
+        id: 2,
+        name: 'Americano', 
+        active: true, 
+        keyImagePath:'./img/menu/americano.jpg', 
+        shortDesc: 'Espresso shots and hot water with light layer of crema on top of it. It gives a little stronger flaovr and caffeine kick.',
+        calories: '15',
+        totalFat: '0',
+        totalCarb: '2',
+        nutritionDesc: 'Adults and youth(ages 13 and older) need ann average of 2,000 calories a day, and childerne(ages 4 to 12) need an average of 1,500 calories a day. However, individual needs vary.',
+        price: '$3.50',
+    },
+    {
+        id: 3, 
+        name: 'Cappuccino', 
+        active: true, 
+        keyImagePath:'./img/menu/capuccino.jpg', 
+        shortDesc: 'Dark Roast Espresso bean with smoothed layer of thick milk form on top. Try with slight cinanon powder.',
+        calories: '190',
+        totalFat: '8',
+        totalCarb: '19',
+        nutritionDesc: 'Adults and youth(ages 13 and older) need ann average of 2,000 calories a day, and childerne(ages 4 to 12) need an average of 1,500 calories a day. However, individual needs vary.',
+        price: '$4.20'
+    },
+
+
+    {
+        id: 4, 
+        name: 'Latte', 
+        active: true, 
+        keyImagePath:'./img/menu/latte.jpg', 
+        shortDesc: 'Dark Roast Espresso bean with steam milk and light layer of form. Enjoy the customize your latte with other milks and sweetness.',
+        calories: '180',
+        totalFat: '7',
+        totalCarb: '19',
+        nutritionDesc: 'Adults and youth(ages 13 and older) need ann average of 2,000 calories a day, and childerne(ages 4 to 12) need an average of 1,500 calories a day. However, individual needs vary.',
+        price: '$4.50'
+    },
+
+    {
+        id: 5, 
+        name: 'Mocha', 
+        active: true, 
+        keyImagePath:'./img/menu/mocha.jpg', 
+        shortDesc: 'Combine our rich and full espresso with bittersweet mocha sauce and steamed milk, then top of whipped-cream. The best coffee drink to your sweet drink.',
+        calories: '350',
+        totalFat: '15',
+        totalCarb: '44',
+        nutritionDesc: 'Adults and youth(ages 13 and older) need ann average of 2,000 calories a day, and childerne(ages 4 to 12) need an average of 1,500 calories a day. However, individual needs vary.',
+        price: '$5.20'
+    },
+
+];
+
+var locationSet =[
+    {
+        id: 0,
+        locationName: 'Burrard',
+        active: true,
+        storeImagePath: './img/location/burrard.jpg',
+        address: '198 Burrard Street, Vancouver',
+    },
+    {
+        id: 1,
+        locationName: 'English Bay',
+        active: true,
+        storeImagePath: './img/location/englishbay.jpg',
+        address: '1234 Davie Street, Vancouver',
+    },
+    {
+        id: 2,
+        locationName: 'Bute',
+        active: true,
+        storeImagePath: './img/location/bute.jpg',
+        address: '234 Bute Street, Vancouver',
+    },
+    {
+        id: 3,
+        locationName: 'Robson',
+        active: true,
+        storeImagePath: './img/location/robson.jpg',
+        address: '563 Robson Street, Vancouver',
+    },
+    {
+        id: 4,
+        locationName: 'Yaletown',
+        active: true,
+        storeImagePath: './img/location/yaletown.jpg',
+        address: '984 Hamilton Street, Vancouver',
+    },
+    {
+        id: 5,
+        locationName: 'Chinatown',
+        active: true,
+        storeImagePath: './img/location/chinatown.jpg',
+        address: '354 Abbott Street, Vancouver',
+    },
+    {
+        id: 6,
+        locationName: 'Main',
+        active: true,
+        storeImagePath: './img/location/main.jpg',
+        address: '158 main Street, Vancouver',
+    },
+    {
+        id: 7,
+        locationName: 'Kingsway',
+        active: true,
+        storeImagePath: './img/location/kingsway.jpg',
+        address: '35 Kingsway Street, Burnaby',
+    }
+
+
+];
+
